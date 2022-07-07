cfcb12580809a2cd27e5be25a2aec187b704a62c
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-07-07 12:04:33 -0400

    Initial commit pt. 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import logo from './logos/logo.png';
 import './App.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <img src = {logo} class = 'center'></img>
+    <div className="container" style = {{backgroundColor: '#898AA6', maxWidth: '350px', padding: '20px', margin: 'auto', marginTop: '30px'}}>
+      <h1 style = {{color: 'azure', display: 'flex', justifyContent: 'center', fontSize: '60px'}}>Career</h1>
     </div>
+    
+    <div className = 'container' style = {{backgroundColor: '#D6EFED', width: '800px', float: 'left', marginLeft: '80px'}}>
+      <h1 style = {{color: '', fontSize: '50px', display: 'flex', justifyContent: 'center'}}>Teacher</h1>
+      <h1 style = {{color: '#748DA6', fontSize: '25px'}}>Part-time opportunity</h1>
+      <i class="bi bi-app"></i>
+    </div>
+    <div className = 'container' style = {{backgroundColor: '#D6EFED', width: '800px', position: 'fixed', left: '50%', marginLeft: '-200px'}}></div>
+    <div className = 'container' style = {{backgroundColor: '#D6EFED', width: '800px', float: 'right', marginRight: '80px'}}></div>
+    </>
   );
 }
 
